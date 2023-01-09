cf4a9e6b8e685a00fedf08ce8031c5fddb3ace11
Astrogene <74155671+Astrogene@users.noreply.github.com>
2023-01-09 15:00:15 +0100

    update login

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import { useAuthStore } from "../stores/auth";
-
+import { useAuthStore } from '../stores/auth';
 
 interface loginForm {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 let loginForm: loginForm = {
-    username: "",
-    password: "",
+  username: '',
+  password: '',
 };
 
 /**
@@ -17,32 +16,45 @@ let loginForm: loginForm = {
  * Else display alert error
  */
 function login() {
-    if (process.server){
-        return
-    }
-    const authStore = useAuthStore();
-    const router = useRouter();
-    authStore
-        .login(loginForm)
-        .then((_response) => {
-            router.push("/")
-        })
-        .catch((error) => console.log("API error", error));
+  if (process.server) {
+    return;
+  }
+  const authStore = useAuthStore();
+  const router = useRouter();
+  authStore
+    .login(loginForm)
+    .then((_response) => {
+      router.push('/');
+    })
+    .catch((error) => console.log('API error', error));
 }
 </script>
 
 <template>
-    <div class="flex w-full h-full flex-column">
-        <form class="w-full h-full mx-auto my-4" @submit.prevent="login">
-            <div class="my-4 username">
-                <label>Username</label>
-                <input v-model="loginForm.username" required type="username" />
-            </div>
-            <div class="my-4 password">
-                <label>Password</label>
-                <input v-model="loginForm.password" required type="password" />
-            </div>
-            <button type="submit">Login</button>
-        </form>
-    </div>
+  <div class="w-full h-full">
+    <form
+      class="flex flex-col justify-center items-center my-4 h-min w-full"
+      @submit.prevent="login"
+    >
+      <div class="my-4 username">
+        <input
+          v-model="loginForm.username"
+          required
+          type="username"
+          class="border-b-4"
+          placeholder="username"
+        />
+      </div>
+      <div class="my-4 password">
+        <input
+          v-model="loginForm.password"
+          required
+          type="password"
+          class="border-b-4"
+          placeholder="password"
+        />
+      </div>
+      <button type="submit" class="rounded-xl border-4 py-4 px-16">Login</button>
+    </form>
+  </div>
 </template>
