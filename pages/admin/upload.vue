05f139dd72fba65744f4bd444072e0714b25e505
astrogene <astrogene@outlook.com>
2023-01-13 19:24:54 +0100

    Upload added

---
+++ pages/admin/upload.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-file-input outlined type="file" v-model="image" name="filename" accept="image/*" @change="onFilePicked" />
+    <v-img class="child-img" v-bind:src="image" :style="{ backgroundImage: 'url(' + previewFiles + ')'}"></v-img>
+</template>
+<script setup lang="ts">
+async function onFilePicked(e: any){
+    const file = e;
+    if(!file) return;
+    const readData = (f) => {
+        new Promise((resolve) => {
+            const reader = new FileReader()
+            reader.onloadend = () =>
+        })
+    }
+
+}
+
+</script>
