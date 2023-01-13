9c2b022a09eea85337a69f95a00b0891b3382360
Astrogene <74155671+Astrogene@users.noreply.github.com>
2023-01-13 11:30:34 +0100

    5 files modified, 1 added, 1 deleted

--- models/database.ts
+++ models/database.ts
@@ -1,19 +1,8 @@
-import User from './User'
-import { Sequelize, DataTypes } from 'sequelize'
+import { Sequelize } from 'sequelize';
 
-export async function Users(){
-    var sequelize = new Sequelize({
-        dialect: 'sqlite',
-        storage: '../data/users.db'
-    });
-    var dbUser = User(sequelize, DataTypes);
-    sequelize.sync()
-    await dbUser.schema.update({
-        username: "admin_secure",
-        password: "secure",
-        role: "ADMIN"
-    }, { where: {
-        id: 0
-    }})
-    return dbUser.schema
-}
+var sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '../data/users.db',
+});
+sequelize.sync();
+export default sequelize;
--- server/api/auth/login.ts
+++ server/api/auth/login.ts
@@ -1,33 +1,40 @@
-import { Users } from '../../../models/database'
-import jwt from 'jsonwebtoken'
+import Users from '~/models/Users';
+import jwt from 'jsonwebtoken';
 
 export default defineEventHandler(async (event) => {
-    const runtimeConfig = useRuntimeConfig()
-    const body = await readBody(event);
-    const user: any = await (await Users()).findOne({
-        where: {
-            username: body.username
-        }
-    });
-    if (user){
-        //TODO FIX
-        if (user.isValidPassword(body.password)){
-            const jwt_access = jwt.sign({
-                id: user.id,
-                auth_level: user.role,
-                name: user.username
-            }, runtimeConfig.secret, { expiresIn: '1m' });
-            const jwt_refresh = jwt.sign({
-                id: user.id,
-                auth_level: user.role,
-            }, runtimeConfig.secret, { expiresIn: '5h' });
-            const res = {
-                jwt_access: jwt_access,
-                jwt_refresh: jwt_refresh
-            }
-            return res;
-        }
+  const runtimeConfig = useRuntimeConfig();
+  const body = await readBody(event);
+  const user: any = await Users.findOne({
+    where: {
+      username: body.username,
+    },
+  });
+  if (user) {
+    //TODO FIX
+    if (user.isValidPassword(body.password)) {
+      const jwt_access = jwt.sign(
+        {
+          id: user.id,
+          auth_level: user.role,
+          name: user.username,
+        },
+        runtimeConfig.secret,
+        { expiresIn: '1m' }
+      );
+      const jwt_refresh = jwt.sign(
+        {
+          id: user.id,
+          auth_level: user.role,
+        },
+        runtimeConfig.secret,
+        { expiresIn: '5h' }
+      );
+      const res = {
+        jwt_access: jwt_access,
+        jwt_refresh: jwt_refresh,
+      };
+      return res;
     }
-    return null;
-
+  }
+  return null;
 });
