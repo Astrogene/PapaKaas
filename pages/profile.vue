6ba251291a0d37edfe1cf25019d6afd62b1af263
astrogene <astrogene@outlook.com>
2023-03-19 14:28:05 +0100

    profile start

---
+++ pages/profile.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="py-4 text-5xl text-center">Hi! {{ user.name }}</h1>
+    <p class="text-xl text-center">Role: {{ user.role }}</p>
+</template>
+<script setup lang="ts">
+const { user }= useAuth()
+if (!user.loggedIn) navigateTo('/auth/login')
+</script>
